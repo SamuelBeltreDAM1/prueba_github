8393ecf0f4a79fb68f9937d4b864afc9b03b4559
SamuelBeltreDAM1 <samubelme34ec@linkiafp.online>
2024-01-09 10:03:08 +0100

    inicializamos el Scanner

--- prubas_github/src/principal/EJERCICIO1.java
+++ prubas_github/src/principal/EJERCICIO1.java
@@ -1,10 +1,13 @@
 package principal;
 
+import java.util.Scanner;
+
 public class EJERCICIO1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ int [] asientos = new int [0];
+ Scanner sc = new Scanner (System.in);
 	}
 
 }
